b2110f16e12e618b4efaddc374144808f151272e
Petrus Nogueira <petrus.wejam@gmail.com>
2021-09-09 23:05:00 -0300

    Tornando a interface responsiva

--- src/payment-fields.php
+++ src/payment-fields.php
@@ -1,8 +1,8 @@
 <div id="pague-logo-screen">
     <div class='card-wrapper'></div>
     <fieldset id="payment-fields">
-        <input type="text" name="number" placeholder="•••• •••• •••• ••••">
         <input type="text" name="name" placeholder="Nome completo"/>
+        <input type="text" name="number" placeholder="•••• •••• •••• ••••">
         <input type="text" name="expiry" placeholder="••/••"/>
         <input type="text" name="cvc" placeholder="CVV"/>
     </fieldset>
